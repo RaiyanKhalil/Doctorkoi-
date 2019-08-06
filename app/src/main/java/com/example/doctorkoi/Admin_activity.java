202808d358fc64d1ad3a57ecd4a40dfb771b6eaf
RaiyanKhalil <raiyankhalil@gmail.com>
2019-08-06 17:34:50 +0600

    second

--- app/src/main/java/com/example/doctorkoi/Admin_activity.java
+++ app/src/main/java/com/example/doctorkoi/Admin_activity.java
@@ -25,8 +25,15 @@ public class Admin_activity extends AppCompatActivity {
     public void addRecord(View view) {
         DbManager db = new DbManager(this);
 
+
+        if(( t1.getText().toString().length() == 0 ) ||( t2.getText().toString().length() == 0 ) || ( t3.getText().toString().length() == 0 )){
+            t1.setError( "First name is required!" );
+            t2.setError( "Specialty name is required!" );
+            t3.setError( "Area name is required!" );}
+    else {
         String res = db.addRecord(t1.getText().toString(), t2.getText().toString(), t3.getText().toString());
-        Toast.makeText(this, res, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, res, Toast.LENGTH_LONG).show();}
+
 
         t1.setText("");
         t2.setText("");
